16074df323096d38a7e6a5175c66506514c4c210
Gustavo Fernandes <gustavonalle@gmail.com>
2017-10-31 08:27:52 +0000

    Mark proto entity as indexed

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -5,5 +5,5 @@ for f in ../data/characters/*.json
 do 
   curl -u user:user -XPOST --data-binary @${f}  -H "Content-Type: application/json"  http://127.0.0.1:8080/rest/indexed/$(basename $f .json)
   let status=status+1
-  echo  "Imported $status characters\r"
+  echo  "Imported $status characters"
 done
